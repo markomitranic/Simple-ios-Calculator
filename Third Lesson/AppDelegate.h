f0078f80729ff23aaa5b5b90a9152344e39f50c0
Marko Mitranić <marko.mitranic@me.com>
2015-12-15 18:11:44 +0100

    Initial Commit

---
+++ Third Lesson/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Third Lesson
+//
+//  Created by Marko Mitranić on 12/15/15.
+//  Copyright © 2015 Marko Mitranić. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Third Lesson/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Third Lesson
+//
+//  Created by Marko Mitranić on 12/15/15.
+//  Copyright © 2015 Marko Mitranić. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
